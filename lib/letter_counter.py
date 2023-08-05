03cc938a454969ec321c017ccc9f4116892816a2
sarahellis <sarahellislondon@gmail.com>
2023-08-05 18:19:54 +0100

    Trying out VS Code Python Interactive Debugger

---
+++ lib/letter_counter.py
@@ -0,0 +1,51 @@
+# Exercise - identifying bugs with VS Code Python interactive debugger
+
+class LetterCounter:
+    def __init__(self, text):
+        self.text = text
+
+    def calculate_most_common(self):
+        counter = {}
+        most_common = None
+        most_common_count = 0 
+        for char in self.text:
+            if not char.isalpha():
+                continue
+            counter[char] = counter.get(char, 0) + 1 
+            if counter[char] > most_common_count:
+                most_common = char
+                most_common_count = counter[char] 
+        return [most_common_count, most_common]
+
+
+counter = LetterCounter("Digital Punk")
+print(counter.calculate_most_common())
+# Intended output:
+# [2, "i"]
+# Actual output:
+# [3, 'D']
+
+
+## Original, with bugs:
+# class LetterCounter:
+#     def __init__(self, text):
+#         self.text = text
+
+#     def calculate_most_common(self):
+#         counter = {}
+#         most_common = None
+#         most_common_count = 1                         # BUG? Why start a 1?
+#         for char in self.text:
+#             if not char.isalpha():
+#                 continue
+#             counter[char] = counter.get(char, 1) + 1  # BUG here, setting default value to 1 rather than 0, in get()
+#             if counter[char] > most_common_count:
+#                 most_common = char
+#                 most_common_count += counter[char]    # BUG here (only need to reset var to counter[char] not add it to existing value)
+#         return [most_common_count, most_common]
+
+# Out of curiosity, where joint common letters:
+counter = LetterCounter("Mississippi")  # Hmm, only shows 's' not 's' and 'i'
+print(counter.calculate_most_common()) 
+# Output:
+# [4, 's']

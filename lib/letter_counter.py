15a5c3e21ec5bd7ee5958fc43d350ddec988db07
sarahellis <sarahellislondon@gmail.com>
2023-08-05 18:26:43 +0100

    Updated with improved code to fix bugs

--- lib/letter_counter.py
+++ lib/letter_counter.py
@@ -49,3 +49,34 @@ counter = LetterCounter("Mississippi")  # Hmm, only shows 's' not 's' and 'i'
 print(counter.calculate_most_common()) 
 # Output:
 # [4, 's']
+
+# ChatGPT's improved code to address case where there's more than one common letter
+# & it also identified a 4th bug -
+# code doesn't handle the case where there's no alpha chars in 'text' input.
+# Now returns [4, ['i', 's']] for Mississipi 'text' input.
+
+class LetterCounter:
+    def __init__(self, text):
+        self.text = text
+
+    def calculate_most_common(self):
+        counter = {}
+        most_common_letters = []
+        most_common_count = 0
+        
+        for char in self.text:
+            if not char.isalpha():
+                continue
+            counter[char] = counter.get(char, 0) + 1
+            if counter[char] > most_common_count:
+                most_common_letters = [char]
+                most_common_count = counter[char]
+            elif counter[char] == most_common_count:
+                most_common_letters.append(char)
+
+        # Handle 'no alpha chars found'
+        if most_common_count == 0:
+            return [0, None]
+
+        return [most_common_count, most_common_letters]
+
